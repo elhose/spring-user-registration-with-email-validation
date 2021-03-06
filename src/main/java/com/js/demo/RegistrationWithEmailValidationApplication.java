853728d908b75df7465156c8e4d6618f702a6dcd
Jacek Sawko <jaceksawko95@gmail.com>
2021-03-06 19:20:12 +0100

    initial commit

---
+++ src/main/java/com/js/demo/RegistrationWithEmailValidationApplication.java
@@ -0,0 +1,13 @@
+package com.js.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationWithEmailValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationWithEmailValidationApplication.class, args);
+    }
+
+}
