f3a6075dfcbbdaaa7a526c368ea24a85d4447048
Jacek <jaceksawko95@gmail.com>
2021-03-07 17:00:53 +0100

    Created RegistrationController.java with stub of a RegistrationService.java, just to make sure, everything works as it is supposed to (also created RegistrationRequest.java as a DTO object)

---
+++ src/main/java/com/js/demo/registration/RegistrationRequest.java
@@ -0,0 +1,11 @@
+package com.js.demo.registration;
+
+import lombok.Data;
+
+@Data
+public class RegistrationRequest {
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String password;
+}
