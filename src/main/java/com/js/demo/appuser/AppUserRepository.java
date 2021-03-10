d14def67f9896f928775d07b9af1c47b07117840
Jacek <jaceksawko95@gmail.com>
2021-03-11 00:27:19 +0100

    Added another method in AppUserRepository.java, it sets, @Modifying annotation is necessary ,because UPDATE is DML

--- src/main/java/com/js/demo/appuser/AppUserRepository.java
+++ src/main/java/com/js/demo/appuser/AppUserRepository.java
@@ -1,6 +1,8 @@
 package com.js.demo.appuser;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,4 +14,7 @@ public interface AppUserRepository extends JpaRepository<AppUser, Long> {
 
     Optional<AppUser> findByEmail(String email);
 
+    @Modifying
+    @Query("UPDATE AppUser a SET a.enabled = true WHERE a.email = ?1")
+    int enableAppUserByMail(String email);
 }
