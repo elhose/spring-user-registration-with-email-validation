6ddc214f8930e7e71756fadb9dd555ad1f6c691e
Jacek <jaceksawko95@gmail.com>
2021-03-11 00:36:22 +0100

    Implemented confirmToken() method, to handle verifying, setting Confirmation and setting that in appUserService entity

--- src/main/java/com/js/demo/registration/RegistrationService.java
+++ src/main/java/com/js/demo/registration/RegistrationService.java
@@ -3,8 +3,14 @@ package com.js.demo.registration;
 import com.js.demo.appuser.AppUser;
 import com.js.demo.appuser.AppUserRole;
 import com.js.demo.appuser.AppUserService;
+import com.js.demo.registration.token.ConfirmationToken;
+import com.js.demo.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Service
 @AllArgsConstructor
@@ -12,6 +18,7 @@ public class RegistrationService {
 
     private final EmailValidator emailValidator;
     private final AppUserService appUserService;
+    private final ConfirmationTokenService confirmationTokenService;
 
     public String register(RegistrationRequest request) {
         boolean isValidEmail = emailValidator.test(request.getEmail());
@@ -26,5 +33,24 @@ public class RegistrationService {
                 request.getPassword(),
                 AppUserRole.USER
         ));
+    }
+
+    @Transactional
+    public String confirmToken(String token) {
+        ConfirmationToken confirmationToken = confirmationTokenService.getConfirmationTokenByToken(token)
+                                                                      .orElseThrow(() -> new IllegalStateException("Token not found!"));
+
+        if (Objects.nonNull(confirmationToken.getConfirmedAt())) {
+            throw new IllegalStateException("Email already confirmed!");
+        }
+
+        if (confirmationToken.getExpiresAt().isBefore(LocalDateTime.now())) {
+            throw new IllegalStateException("Activation token already expired!");
+        }
+
+        confirmationToken.setConfirmedAt(LocalDateTime.now());
+        appUserService.enableAppUserByEmail(confirmationToken.getAppUser().getEmail());
+
+        return "Email " + confirmationToken.getAppUser().getEmail() + " Confirmed!";
     }
 }
